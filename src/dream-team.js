92ef6da339a881867536484cd536cbdbd49d6191
Ilyas <ilyasdossanov@gmail.com>
2021-03-11 20:47:26 +0600

    fead: add dream-team function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+	try{
+	  let shortArr = [];
+	  let teamName ='';
+	  for(let i = 0; i<members.length; i++){
+	    if(typeof members[i] == "string"){
+	    	for(let k = 0; k<members[i].length; k++){
+	    		if (members[i].charAt(k) != ' ') {
+	    			shortArr.push(members[i].charAt(k).toUpperCase());
+	    			break;
+	    		}
+	    	}
+	    }
+	  }
+    shortArr.sort()
+	  for(j = 0; j<shortArr.length; j++){
+	  	teamName += shortArr[j];
+	  }
+	  return(teamName);
+	}
+	catch(TypeError){
+	return(false);
+}
 };
